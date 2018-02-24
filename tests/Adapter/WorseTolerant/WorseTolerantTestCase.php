73620cbc057bd91707346f1ce8f8ccea796b3e60
dantleech <dan.t.leech@gmail.com>
2018-02-24 14:50:01 +0100

    Updated to latest WR

--- tests/Adapter/WorseTolerant/WorseTolerantTestCase.php
+++ tests/Adapter/WorseTolerant/WorseTolerantTestCase.php
@@ -6,15 +6,15 @@ use PHPUnit\Framework\TestCase;
 use Phpactor\ClassMover\Domain\MemberFinder;
 use Phpactor\WorseReflection\Core\SourceCodeLocator\StringSourceLocator;
 use Phpactor\ClassMover\Adapter\WorseTolerant\WorseTolerantMemberFinder;
-use Phpactor\WorseReflection\Reflector;
 use Phpactor\WorseReflection\Core\SourceCode;
+use Phpactor\WorseReflection\ReflectorBuilder;
 
 abstract class WorseTolerantTestCase extends TestCase
 {
     protected function createFinder(string $source): MemberFinder
     {
-        $locator = new StringSourceLocator(SourceCode::fromString($source));
-
-        return new WorseTolerantMemberFinder(Reflector::create($locator));
+        return new WorseTolerantMemberFinder(
+            ReflectorBuilder::create()->addSource($source)->build()
+        );
     }
 }
